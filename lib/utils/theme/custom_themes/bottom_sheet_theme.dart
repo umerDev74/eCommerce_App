4e0cbc219deaae597da84112f785ea64b96265fc
umerDev74 <umern7429@gmail.com>
2026-06-13 17:14:49 +0500

    thre more custom Themes are added

---
+++ lib/utils/theme/custom_themes/bottom_sheet_theme.dart
@@ -0,0 +1,26 @@
+
+import 'package:flutter/material.dart';
+
+class TBottomSheetTheme {
+  TBottomSheetTheme._();
+
+  // ----- LIGHT BOTTOM_SHEET_THEME -------------
+
+  static BottomSheetThemeData lightBottomSheetTheme = BottomSheetThemeData(
+    showDragHandle: true,
+    backgroundColor: Colors.white,
+    modalBackgroundColor: Colors.white,
+    constraints: const BoxConstraints(minWidth: double.infinity),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+  );
+
+  // ----- DARK BOTTOM_SHEET_THEME -------------
+
+  static BottomSheetThemeData darkBottomSheetTheme = BottomSheetThemeData(
+    showDragHandle: true,
+    backgroundColor: Colors.black,
+    modalBackgroundColor: Colors.black,
+    constraints: const BoxConstraints(minWidth: double.infinity),
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+  );
+}
