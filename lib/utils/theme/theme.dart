3db96d6ba0c489064e524b0edf5960d273766beb
umerDev74 <umern7429@gmail.com>
2026-06-13 12:22:31 +0500

    create themes for dark and light theme

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,27 @@
+
+import 'package:ecommerce_app/utils/theme/custom_themes/elevated_button_theme.dart';
+import 'package:ecommerce_app/utils/theme/custom_themes/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.LightElevatedButtonTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+      useMaterial3: true,
+      fontFamily: 'poppins',
+      brightness: Brightness.dark,
+      primaryColor: Colors.blue,
+      scaffoldBackgroundColor: Colors.black,
+      textTheme: TTextTheme.darkTextTheme,
+      elevatedButtonTheme: TElevatedButtonTheme.DarkElevatedButtonTheme,
+  );
+}
